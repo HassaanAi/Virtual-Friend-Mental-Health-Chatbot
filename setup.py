7acd702bfd6748c87a892a6bd156995f657eab74
HassaanAi <hassaanxhk11@gmail.com>
2024-12-21 14:05:20 +0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Virtual Friend Mental Health Chatbot',
+    version= '0.0.0',
+    author= 'Hassaan & Kanza',
+    author_email= 'hassaanxhk11@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
